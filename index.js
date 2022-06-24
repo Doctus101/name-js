38d0570d90d9ae559e6189f72297f9bbbe8c7b1d
Prudentus <prudoctus01@gmail.com>
2022-06-24 12:40:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var myname = "Illo Uche";
+var myheight = "1.63m";
+var country = "Nigeria";
+
+alert(
+  "Hello! I am" +
+    " " +
+    myname +
+    " " +
+    "I am " +
+    myheight +
+    " tall" +
+    " and I am from " +
+    country +
+    "."
+);
